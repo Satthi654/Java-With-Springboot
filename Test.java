4fa67f604720a4307b9cbe6de8984f5172893c32
Satthi654 <Satthi654@gmail.com>
2023-02-21 14:57:57 +0530

    First Commit

---
+++ Test.java
@@ -0,0 +1,11 @@
+public class Test{
+	public static void main(String[] args)
+	{
+		System.out.println("Executed The main method from Test class");
+	}
+	
+	public static void performMethodOne(){
+		
+		System.out.println("performMethodOne Execution Completed");
+	}
+}
